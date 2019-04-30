17741d78a3994abade945fecefcb589ba7a13adf
zhoulh0322 <zhoulh0322@gmail.com>
2019-04-30 14:00:30 +0800

    new

---
+++ learncpp/helloword.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ctime>
+// #include <ostring>
+
+int main() {
+	std::cout << "Hello, Word!\n";
+	std::cin.get();
+	while (true) {
+		std::cout << "yes";
+	};
+	return 0;
+}
